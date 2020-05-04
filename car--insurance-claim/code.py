1ec075e444fa74253a70bb91853a49564a2362fd
ankitaduttagupta <ankitaduttagupta@users.noreply.github.com>
2020-05-04 13:02:05 +0530

    Code file created.

---
+++ car--insurance-claim/code.py
@@ -0,0 +1,118 @@
+# --------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random as rd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+df= pd.read_csv(path)
+df.head(5 )
+df=df.replace({'\$':'',',':''},regex=True)
+y=df['CLAIM_FLAG'].copy()
+X=df.drop('CLAIM_FLAG',1)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size = 0.3,random_state = 6)
+
+
+# --------------
+# Code starts here
+X_train[['INCOME','HOME_VAL','BLUEBOOK','OLDCLAIM','CLM_AMT']]=X_train[['INCOME','HOME_VAL','BLUEBOOK','OLDCLAIM','CLM_AMT']].astype(float)
+
+X_test[['INCOME','HOME_VAL','BLUEBOOK','OLDCLAIM','CLM_AMT']]=X_test[['INCOME','HOME_VAL','BLUEBOOK','OLDCLAIM','CLM_AMT']].astype(float)
+print(X_test.isnull().sum(),X_train.isnull().sum())
+
+
+
+# Code ends here
+
+
+# --------------
+
+# drop missing values
+X_train.dropna(subset=['YOJ','OCCUPATION'],inplace=True)
+X_test.dropna(subset=['YOJ','OCCUPATION'],inplace=True)
+
+
+y_train=y_train[X_train.index]
+y_test=y_test[X_test.index]
+
+
+
+# fill missing values with mean
+X_train['AGE'].fillna((X_train['AGE'].mean()), inplace=True)
+X_test['AGE'].fillna((X_train['AGE'].mean()), inplace=True)
+
+X_train['CAR_AGE'].fillna((X_train['CAR_AGE'].mean()), inplace=True)
+X_test['CAR_AGE'].fillna((X_train['CAR_AGE'].mean()), inplace=True)
+
+
+
+X_train['INCOME'].fillna((X_train['INCOME'].mean()), inplace=True)
+X_test['INCOME'].fillna((X_train['INCOME'].mean()), inplace=True)
+
+
+
+X_train['HOME_VAL'].fillna((X_train['HOME_VAL'].mean()), inplace=True)
+X_test['HOME_VAL'].fillna((X_train['HOME_VAL'].mean()), inplace=True)
+
+
+print(X_train.isnull().sum())
+print(X_test.isnull().sum())
+
+
+# --------------
+from sklearn.preprocessing import LabelEncoder
+columns = ["PARENT1","MSTATUS","GENDER","EDUCATION","OCCUPATION","CAR_USE","CAR_TYPE","RED_CAR","REVOKED"]
+
+# Code starts here
+for i in columns:
+    le=LabelEncoder()
+    X_train[i]=le.fit_transform(X_train[i].astype(str))
+    X_test[i]=le.transform(X_test[i].astype(str))
+    
+
+# Code ends here
+
+
+
+# --------------
+from sklearn.metrics import precision_score 
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+
+
+# code starts here 
+model=LogisticRegression(random_state=6)
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+score=accuracy_score(y_pred,y_test)
+# Code ends here
+
+
+# --------------
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+
+# code starts here
+smote=SMOTE(random_state=9)
+X_train,y_train=smote.fit_sample(X_train,y_train)
+
+scaler=StandardScaler()
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)
+
+
+# Code ends here
+
+
+# --------------
+# Code Starts here
+model=LogisticRegression()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+score=accuracy_score(y_pred,y_test)
+# Code ends here
+
+
